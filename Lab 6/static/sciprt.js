102d1fa5a84d0f3d441d3b9bc0253c06e2817cef
ProfNaama <nami@cmu.edu>
2025-03-25 10:52:17 -0400

    adding rwd

--- Lab 6/static/sciprt.js
+++ Lab 6/static/sciprt.js
@@ -55,3 +55,13 @@ AllLinks.forEach(
         }
     }
 )
+
+
+function myFunction() {
+    var x = document.getElementById("navbar");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
