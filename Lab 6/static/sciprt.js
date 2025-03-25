b37699b4136c84704893381a33326f2b68058742
ProfNaama <nami@cmu.edu>
2025-03-25 07:26:01 -0400

    creating day 6

---
+++ Lab 6/static/sciprt.js
@@ -0,0 +1,57 @@
+// {
+//     var x = 5;
+//     var y = 7;
+//     var z = x+y;
+//     console.log("hi");
+//     console.log(z);
+//     window.alert("hi"+ z)
+// }
+
+
+// var x = 5;
+// var y = 7;
+
+// function MyFirstFunction(x,y) {
+//     return x+y;
+// }
+// var z = MyFirstFunction(2,3);
+// var z = MyFirstFunction(4456,8890)
+
+
+
+// var d = new Date();
+// var h = d.getHours();
+// // window.alert(d);
+// // window.alert(h);
+
+
+// var student = {name:"Noah", age:19}
+
+// if (h=16) {
+//     // window.alert("good afternoon")
+//     document.getElementById("123").innerHTML = "good afternoon " + student.name + "!";
+    
+// }
+
+// links = document.getElementsByName('a');
+// newlinks = Array.from(links);
+// console.log(newlinks);
+// console.log(window.location);
+
+// links.forEach(element => {
+//     console.log(element);
+// });
+
+
+
+const AllLinks = document.querySelectorAll('nav a');
+console.log(AllLinks);
+
+AllLinks.forEach(
+    MyLink => {
+        if (MyLink.href.includes(window.location.pathname)) {
+            MyLink.classList.add("active");
+            console.log("I am in");
+        }
+    }
+)
